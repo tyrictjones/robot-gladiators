969715d1caf17dae19ebd0229b56e09bc7f18d90
Tyric Jones <tyrictjones@gmail.com>
2022-08-04 23:13:54 -0700

    array for enemy names

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,11 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     //Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -74,4 +72,4 @@ var fight = function() {
     }
 };
 
-fight();
+// fight();
