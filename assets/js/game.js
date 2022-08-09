44e1e104f0a04d980453202ddf59489d55ddec67
Tyric Jones <tyrictjones@gmail.com>
2022-08-08 21:39:18 -0700

    allow mixed case skip entry

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,42 @@
-var fight = function(enemy) {
-    while (playerInfo.health > 0 && enemy.health > 0) {
-        // ask player if they'd like to fight or run
-        var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+//function to fight or skip the round
+var fightOrSkip = function() {
+    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
     
+        //Recursive function call if response is blank or null
+        if (!promptFight) {
+            window.alert("You need to provide a valid answer! Please try again.");
+            return fightOrSkip();
+        }
+
+        promptFight = promptFight.toLowerCase();
+        
         // if player picks "skip" confirm and then stop the loop
-        if (promptFight === "skip" || promptFight === "SKIP") {
+        if (promptFight === "skip") {
           // confirm player wants to skip
-          var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+          var confirmSkip = window.confirm("Are you sure you would like to quit?");
     
-          // if yes (true), leave fight
-          if (confirmSkip) {
+        // if yes (true), leave fight
+        if (confirmSkip) {
             window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
             // subtract money from playerInfo.money for skipping
             playerInfo.money = Math.max(0, playerInfo.money - 10);
-            console.log("playerInfo.money", playerInfo.money)
+
+            //return true if the player wants to leave
+            return true;
+        }
+    }
+
+    return false;
+};//end of fight or skip function
+
+
+
+var fight = function(enemy) {
+    while (playerInfo.health > 0 && enemy.health > 0) {
+        // ask player if they'd like to fight or run
+        if (fightOrSkip()) {
+            //if true, leave fight by breaking loop
             break;
-          }
         }
     
         // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
